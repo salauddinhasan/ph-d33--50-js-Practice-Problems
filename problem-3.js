a49ab6ff99b9cb7c4a3d4682d31d80eb1706edc0
salauddin <salauddin@hasan.com>
2026-02-08 10:36:21 +0600

    determine student grade based on marks

---
+++ problem-3.js
@@ -0,0 +1,19 @@
+//  Student এর mark দিয়ে grade বের করো
+
+let mark = 29;
+
+if(mark >= 80) {
+    console.log('A+');
+} else if(mark >= 70){
+    console.log('A');
+} else if(mark >= 60) {
+    console.log('A-')
+} else if(mark >= 50) {
+    console.log('B');
+} else if(mark >= 40) {
+    console.log('B-');
+} else if(mark >= 33) {
+    console.log('D');
+} else {
+    console.log('Fall');
+}
