48fc5280e2747817c8f80733061c70346d4e8006
salauddin <salauddin@hasan.com>
2026-02-08 10:08:49 +0600

    check if a number is greater than, less than, or equal to 100

---
+++ problem-2.js
@@ -0,0 +1,11 @@
+// একটি সংখ্যা বড় না ছোট 100 থেকে চেক করো
+
+let number = 10;
+
+if(number > 100) {
+    console.log('Number 100 theke boro');
+} else if(number < 100) {
+    console.log('Number 100 theke choto ');
+} else {
+    console.log('equal')
+};
