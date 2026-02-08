e4414838b7779bbfafb34648402b38e2b31d5c94
salauddin <salauddin@hasan.com>
2026-02-08 11:02:34 +0600

    create login system with email and password check

---
+++ problem-6.js
@@ -0,0 +1,14 @@
+// Login system বানাও (email && password চেক)
+
+
+let email = 'rakib@gmail.com';
+let password = '12345';
+ 
+let inputEmail = 'rakib@gmail.com';
+let inputPassword = '12345';
+
+if(inputEmail === email && inputPassword === password) {
+    console.log('Login Successful');
+} else{
+    console.log('Login Failed');
+}
