c122f44a39033544343ced877de7e05c3acb7f31
salauddin <salauddin@hasan.com>
2026-02-08 10:00:49 +0600

    check if a person is eligible to vote based on age

---
+++ Variable & Data Type/problem-1.js
@@ -0,0 +1,14 @@
+//  let, const, var দিয়ে একই ভ্যারিয়েবল declare করে পার্থক্য দেখাও
+
+
+// let = Value পরিবর্তন করা যায়
+// Block-scoped → শুধু যেই block {} এর ভিতরে declare করা হয়েছে, ওখানেই কাজ করে
+
+
+// const = Value পরিবর্তন করা যায় না
+// Block-scoped
+
+
+// var = Value পরিবর্তন করা যায়
+
+// Function-scoped / globally scoped
--- problem-1.js
+++ problem-1.js
@@ -1,14 +1,11 @@
-//  let, const, var দিয়ে একই ভ্যারিয়েবল declare করে পার্থক্য দেখাও
+ 
+
+//  বয়স দিয়ে vote দিতে পারবে কিনা চেক করো
 
 
-// let = Value পরিবর্তন করা যায়
-// Block-scoped → শুধু যেই block {} এর ভিতরে declare করা হয়েছে, ওখানেই কাজ করে
-
-
-// const = Value পরিবর্তন করা যায় না
-// Block-scoped
-
-
-// var = Value পরিবর্তন করা যায়
-
-// Function-scoped / globally scoped
+let age = 19;
+if(age >= 18) {
+    console.log('You are adult,  vote dite parbi');
+} else {
+    console.log('vote dite parbe na')
+}
