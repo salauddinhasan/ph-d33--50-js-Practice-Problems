1c6d8ce5a4305694b7ec1783e69938cdd2635ff2
salauddin <salauddin@hasan.com>
2026-02-08 08:40:23 +0600

    check if a number is divisible by both 3 and 5

---
+++ problem-6.js
@@ -0,0 +1,11 @@
+
+
+// একটি সংখ্যা 3 এবং 5 উভয় দিয়ে divisible কিনা চেক করো
+
+let num = 15;
+
+if(num % 3 === 0 && num % 5 === 0) {
+    console.log('divisible by both 3 and 5');
+} else {
+    console.log('Not divisible by both 3 and 5');
+}
