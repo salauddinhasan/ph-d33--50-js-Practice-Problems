1c6955a5dd37b2ad11e6f9d780d84576188a4d87
salauddin <salauddin@hasan.com>
2026-02-08 09:43:43 +0600

    check variable types and convert between string and number

---
+++ problem-3.js
@@ -0,0 +1,15 @@
+//  string কে number এ convert করো
+
+let number = '10';
+console.log(typeof(number));
+console.log(number);
+console.log(parseInt((number)));
+
+// number কে string এ convert করো 
+
+let numbers = 100;
+console.log(String(numbers));
+
+
+let price = 150;
+console.log(price.toString()); 
