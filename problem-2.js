926b6df45e3fa52468fdfca0506688422a7aa989
salauddin <salauddin@hasan.com>
2026-02-09 07:45:58 +0600

    Add program to get first and last elements of an array

---
+++ problem-2.js
@@ -0,0 +1,6 @@
+// array এর length বের করো
+
+
+let subjects = ['Math', 'Python', 'Java', 'JavaScript', 'Html'];
+
+console.log(subjects.length);
---
+++ problem-3.js
@@ -0,0 +1,7 @@
+// array থেকে প্রথম ও শেষ element বের করো
+
+let subjects = ['Math', 'Python', 'Java', 'JavaScript', 'Html'];
+
+ 
+ console.log('first = ', subjects[0]);
+ console.log('last = ', [subjects.length - 1]);
