be39354a15f86dc415637b91300c2732de457a9f
salauddin <salauddin@hasan.com>
2026-02-09 07:01:13 +0600

    Add multiplication table using for loop

---
+++ problem-4.js
@@ -0,0 +1,12 @@
+
+
+// একটি সংখ্যার নামতা print করো
+
+for(let i = 2; i <= 10; i++) {
+     
+    console.log('3  * ', + i + ' = ' + (3 * i));
+}
+
+for(let i = 1; i <= 20; i++) {
+    console.log('5 * ', + i + (5 * i));
+}
