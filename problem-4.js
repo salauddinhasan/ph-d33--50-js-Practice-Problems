14dd7c2de4982f9bf7dd19cf9e33c1ea8281d404
salauddin <salauddin@hasan.com>
2026-02-16 14:58:53 +0600

    Implemented sum logic for subjects array

---
+++ problem-4.js
@@ -0,0 +1,11 @@
+// array এর সব সংখ্যার যোগফল বের করো
+
+
+let subjects = [10, 34, 56, 66, 78];
+let sum = 0;
+
+for(const subject of subjects) {
+    sum = sum + subject;
+    
+}
+console.log(sum);
