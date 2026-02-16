624b7e49b6e228255bc72fa45aff7ffec95b2352
salauddin <salauddin@hasan.com>
2026-02-16 15:10:07 +0600

    Implemented even number extraction using for...of loop

---
+++ problem-5.js
@@ -0,0 +1,24 @@
+// array থেকে even সংখ্যাগুলো বের করো
+
+
+// let numbers = [10, 15, 20, 25, 30, 33, 18, 13];
+
+// for(const number of numbers) {
+//      if(number % 2 === 0) {
+//         console.log(number);
+//      } 
+// }
+
+
+// যদি even number গুলো নতুন array তে রাখতে চাই?
+
+let numbers = [ 12, 34, 54, 35, 67, 95, 45,14, 76];
+const evenNumber = [];
+
+for(const number of numbers) {
+    if(number % 2 === 0) {
+        evenNumber.push(number);
+    }
+}
+
+console.log(evenNumber);
